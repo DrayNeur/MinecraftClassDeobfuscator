d27b469570de4116aa4264518cd8eb4c72840157
DrayNeur <50057580+DrayNeur@users.noreply.github.com>
2020-08-10 22:07:59 +0200

    Add files via upload

---
+++ MinecraftClassDeobfuscator/MinecraftClassDeobfuscator/Mainfrm.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace MinecraftClassDeobfuscator
+{
+    partial class Mainfrm
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.classList = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(12, 401);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(235, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // classList
+            // 
+            this.classList.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.classList.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.classList.ForeColor = System.Drawing.Color.White;
+            this.classList.FormattingEnabled = true;
+            this.classList.ItemHeight = 21;
+            this.classList.Location = new System.Drawing.Point(12, 33);
+            this.classList.Name = "classList";
+            this.classList.Size = new System.Drawing.Size(473, 254);
+            this.classList.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 21);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Classes";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 326);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 21);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Number of classes: 0";
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(12, 352);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(235, 39);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Deobfuscate";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.label3.Location = new System.Drawing.Point(8, 444);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 19);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Path:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(23)))), ((int)(((byte)(23)))));
+            this.ClientSize = new System.Drawing.Size(497, 472);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.classList);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.White;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox classList;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ MinecraftClassDeobfuscator/MinecraftClassDeobfuscator/Mainfrm.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MinecraftClassDeobfuscator
+{
+    public partial class Mainfrm : Form
+    {
+        public Mainfrm()
+        {
+            InitializeComponent();
+        }
+        List<string> classes = new List<string>();
+        List<string> methods = System.IO.File.ReadLines("method.txt").ToList();
+        List<string> fields = System.IO.File.ReadLines("field.txt").ToList();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    classes = DirSearch(fbd.SelectedPath);
+                    label3.Text = "Path: " + fbd.SelectedPath;
+                    foreach (string classe in classes)
+                    {
+                        if(Path.GetExtension(classe) == ".java")
+                            classList.Items.Add(classe.Replace(fbd.SelectedPath, ""));
+                    }
+                    label2.Text = "Number of classes: " + classes.Count();
+                }
+            }
+        }
+        private List<String> DirSearch(string sDir)
+        {
+            List<String> files = new List<String>();
+            try
+            {
+                foreach (string f in Directory.GetFiles(sDir))
+                {
+                    files.Add(f);
+                }
+                foreach (string d in Directory.GetDirectories(sDir))
+                {
+                    files.AddRange(DirSearch(d));
+                }
+            }
+            catch (System.Exception excpt)
+            {
+                MessageBox.Show(excpt.Message);
+            }
+
+            return files;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (string classe in classes)
+            {
+                if (Path.GetExtension(classe) == ".java")
+                {
+                    string str = File.ReadAllText(classe);
+                    foreach (string method in methods)
+                    {
+                        string[] use = method.Split(';');
+                        str = str.Replace(use[0], use[1]);
+                    }
+                    foreach (string field in fields)
+                    {
+                        string[] use = field.Split(';');
+                        str = str.Replace(use[0], use[1]);
+                    }
+                    File.WriteAllText(classe, str);
+                }
+            }
+            MessageBox.Show("Done !");
+        }
+    }
+}
